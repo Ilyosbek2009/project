25149fa568a5f005d0ebf7c2f24fcb16542d1b67
Ilyosbek2009 <ilyosbekobidjonov06@gmail.com>
2022-08-12 22:14:56 +0500

    sdskdsdskds

---
+++ index.js
@@ -0,0 +1,33 @@
+// 7ta data  type bor
+
+
+
+// #1
+// let a = +prompt("son Metr");
+// let b = a * 100 + "santimetr";
+// let c = +prompt("son santimetr");
+// let d = c / 100 + "metr";
+
+// console.log(b);
+// console.log(d);  
+
+
+// #2
+// let a = +prompt("son Tonna");
+// let b = a * 1000 + "Kilogram";
+// let c = +prompt("son Kilogram");
+// let d = c / 1000 + "Tonna";
+
+// console.log(b);
+// console.log(d);
+
+
+// #3
+// let a = +prompt("son Kilobayt");
+// let b = a * 1024 + "bayt";
+// let c = +prompt("son Bayt");
+// let d = c / 1024 + "Kilobayt";
+
+// console.log(b);
+// console.log(d);
+
